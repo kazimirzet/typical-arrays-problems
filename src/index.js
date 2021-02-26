5417e81d3ae4a6cf68dfd47b767d02e01e88ab05
Kazimir <kazimirzet@gmail.com>
2021-02-26 14:33:51 +0300

    feat: finish Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        let result = array.reduce((prev, current) => {
+            if (prev < current) {
+                return prev;
+            } else {
+                return current;
+            }
+        });
+        return result;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        let result = array.reduce((prev, current) => {
+            if (prev > current) {
+                return prev;
+            } else {
+                return current;
+            }
+        });
+        return result;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        let result = array.reduce((sum, current) => {
+            return sum + current;
+        });
+        return result / array.length;
+    }
+};
